552f91bc88da395a2968fd4c3a8cedb95ee38c82
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-06 12:40:25 -0600

    File updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
-    public function shownname($name){
+    public function showname($name){
 
         return view('example',['name'=> $name]);
 
