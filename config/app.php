8ac4dc664612dd41134672e9d35adcf856a4204a
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-21 22:09:39 -0600

    Updated

--- config/app.php
+++ config/app.php
@@ -34,17 +34,6 @@ return [
         // 'store'  => 'redis',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
     'providers' => [
 
         /*
