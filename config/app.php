c324cd4c28da7c5d5d9b50ac32754ee63623a0d7
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-21 22:08:28 -0600

    Updated

--- config/app.php
+++ config/app.php
@@ -12,17 +12,6 @@ return [
 
     'debug' => (bool) env('APP_DEBUG', false),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
-    |
-    */
-
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
