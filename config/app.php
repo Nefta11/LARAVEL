4550b2a29403ae78f428e98a3aa95429c45552f6
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-21 22:08:01 -0600

    Updated

--- config/app.php
+++ config/app.php
@@ -7,29 +7,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
-    |
-    */
 
     'env' => env('APP_ENV', 'production'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
 
     'debug' => (bool) env('APP_DEBUG', false),
 
