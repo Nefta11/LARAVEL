a17e7d468914419c8099018027cb0a1fdf727fb2
Nefta11 <neftaliarturohernandez@gmail.com>
2024-04-21 22:08:15 -0600

    Updated

--- config/app.php
+++ config/app.php
@@ -27,16 +27,7 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
+
 
     'timezone' => 'UTC',
 
